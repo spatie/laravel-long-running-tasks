5204390542fa7be451a12e2a5ec331c99668e8b8
Patrick Organ <patrick@permafrost.dev>
2024-09-12 20:15:51 -0400

    add unit tests

---
+++ tests/Strategies/CheckStrategyTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use Spatie\LongRunningTasks\Strategies\LinearBackoffCheckStrategy;
+use Spatie\LongRunningTasks\Strategies\ExponentialBackoffCheckStrategy;
+use Spatie\LongRunningTasks\Models\LongRunningTaskLogItem;
+
+it('implements a linear backoff strategy', function () {
+    $strategy = new LinearBackoffCheckStrategy();
+
+    $logItem = LongRunningTaskLogItem::factory()->create();
+
+    $logItem->check_frequency_in_seconds = 10;
+    $logItem->attempt = 1;
+
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(10);
+
+    $logItem->attempt = 2;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(20);
+
+    $logItem->attempt = 3;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(30);
+
+    $logItem->attempt = 4;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(40);
+
+    $logItem->attempt = 5;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(50);
+
+    $logItem->attempt = 6;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(60);
+
+    $logItem->attempt = 7;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(60);
+});
+
+it('implements an exponential backoff strategy', function () {
+    $strategy = new ExponentialBackoffCheckStrategy();
+
+    $logItem = LongRunningTaskLogItem::factory()->create();
+
+    $logItem->check_frequency_in_seconds = 10;
+    $logItem->attempt = 1;
+
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(10);
+
+    $logItem->attempt = 2;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(25);
+
+    $logItem->attempt = 3;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(125);
+
+    $logItem->attempt = 4;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(625);
+
+    $logItem->attempt = 5;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(625);
+
+    $logItem->attempt = 6;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(625);
+});
+
+it('implements a standard backoff strategy', function () {
+    $strategy = new Spatie\LongRunningTasks\Strategies\StandardBackoffCheckStrategy();
+
+    $logItem = LongRunningTaskLogItem::factory()->create();
+
+    $logItem->check_frequency_in_seconds = 10;
+    $logItem->attempt = 1;
+
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(10);
+
+    $logItem->attempt = 2;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(60);
+
+    $logItem->attempt = 3;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(120);
+
+    $logItem->attempt = 4;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(300);
+
+    $logItem->attempt = 5;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(600);
+
+    $logItem->attempt = 6;
+    expect($strategy->checkFrequencyInSeconds($logItem))->toBe(600);
+});
